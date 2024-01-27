12bce6a6738195cfae149d65845ef39b728c91ad
gitkick <d17ca087vignesh@gmail.com>
2024-01-27 10:25:19 +0530

    add

---
+++ project/footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">home</a>
+         <!-- <a href="about.php">about</a> -->
+         <a href="shop.php">shop</a>
+         <a href="contact.php">contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">login</a>
+         <!-- <a href="register.php">register</a> -->
+         <a href="cart.php">cart</a>
+         <a href="orders.php">orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +91 9962171919 </p>
+         <!-- <p> <i class="fas fa-phone"></i> +111-222-3333 </p> -->
+         <p> <i class="fas fa-envelope"></i> vigneshvickyc2@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Chennai, India - 600115 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="https://www.facebook.com/profile.php?id=100026323424156"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <!-- <a href=""> <i class="fab fa-twitter"></i> twitter </a> -->
+         <a href="https://www.instagram.com/vignesh.vickii_/"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="https://www.linkedin.com/in/vigneshvl/"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> copyright ©️ <?php echo date('Y'); ?> created by <span>Vignesh M | All rights reserved</span> </p>
+
+</section>
